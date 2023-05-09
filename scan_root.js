7ffe65bda58dc74ff74228671a01548fda2263c0
lapryx <mmloggen@gmail.com>
2023-05-09 23:20:43 +0200

    add check for at least more than 0 ram

--- scan_root.js
+++ scan_root.js
@@ -1,8 +1,8 @@
 /** @param {NS} ns */
-export async function scan_root(ns,hosts) {
+export async function scan_usable(ns,hosts) {
 	let rootHosts = [];
 	for ( let i = 0; i < hosts.length; i++){
-		if (ns.hasRootAccess(hosts[i])){
+		if (ns.hasRootAccess(hosts[i] && ns.getServerMaxRam > 0)){
 			rootHosts.indexOf(hosts[i]) === -1 ? rootHosts.push(hosts[i].toString()) : await ns.sleep(0);
 		}
 	}
